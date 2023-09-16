60a956cf29c8169751413f14b2ca44f91c95449a
crtbusiness <crtbusiness@gmail.com>
2023-09-16 18:52:33 -0400

    Finished tutorial part 8 - third party apps
    Installed Django Debug Toolbar

--- priceCheckSite/priceCheckSite/urls.py
+++ priceCheckSite/priceCheckSite/urls.py
@@ -20,6 +20,7 @@ from django.urls import include, path
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 #Path arguments:
     #route: string that contains a URL pattern, such as 'polls/'
